2ad574aa158470328b2b1f0010b4181f913f8745
mggoes <matheus.ggoes@outlook.com>
2021-06-28 10:50:06 -0300

    Add repl and evaluation examples

---
+++ src/reference/c2_repl_and_main.clj
@@ -0,0 +1,17 @@
+(ns reference.c2-repl-and-main)
+
+;==============================================================
+;CLJ
+;Command clj invoke clojure.main
+;Run a clojure file: clj -M c2_repl_and_main.clj
+(println "Clojure REPL and main")
+
+;Passing arguments
+;Run a clojure file: clj -M c2_repl_and_main.clj arg1 arg2 arg3
+(println *command-line-args*)
+
+;Run using java command: java -cp clojure.jar clojure.main -m reference.c2-repl-and-main
+;https://clojure.github.io/clojure/clojure.main-api.html#clojure.main/main
+(defn -main
+  []
+  (println "Main function"))
---
+++ src/reference/c3_evaluation.clj
@@ -0,0 +1,10 @@
+(ns reference.c3-evaluation)
+
+;==============================================================
+;Empty list
+(println (class ()))
+
+;Evaluating a form
+(eval (println "Test"))
+(eval (def v [1 2 3]))
+(println v)
