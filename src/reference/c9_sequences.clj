e45f800cdadda73aca9a6a678161b9d428ca6ccc
mggoes <matheus.ggoes@outlook.com>
2021-07-07 14:49:10 -0300

    Add seq examples

---
+++ src/reference/c9_sequences.clj
@@ -0,0 +1,11 @@
+(ns reference.c9-sequences)
+
+;==============================================================
+;SEQUENCE
+(println (seq [1 2 3 4 5]))
+(println (lazy-seq [1 2 3 4 5]))
+
+(def s (seq [1 2 3 4 5]))
+(println (first s))
+(println (rest s))
+(println (cons 0 s))
