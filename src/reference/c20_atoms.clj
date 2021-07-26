a9a53153596076f0a5e6a66288d915a6165030d5
mggoes <matheus.ggoes@outlook.com>
2021-07-26 17:38:25 -0300

    Add atom examples

---
+++ src/reference/c20_atoms.clj
@@ -0,0 +1,19 @@
+(ns reference.c20-atoms
+  (:use [clojure.pprint]))
+
+;==============================================================
+;ATOM
+;Similar to refs and agents
+;Does not need transactions to change the value
+(def a (atom {}))
+(pprint a)
+
+;SWAP
+;Changes atom value
+;Receives a function whose return will be the new value of the atom
+(defn change-value
+  [a k v]
+  (assoc a k v))
+
+(swap! a change-value :p1 "Param 1")
+(pprint a)
