a8b9315685d7aacdffe71c0389f7d1e7c45f6dc6
mggoes <matheus.ggoes@outlook.com>
2021-09-08 09:01:40 -0300

    Add examples

---
+++ src/language/others.clj
@@ -0,0 +1,23 @@
+(ns language.others)
+
+;COMPLEMENT
+;Creates a function that receives the same parameters of the original function, executes it, negates the result and returns it.
+(def not-empty? (complement empty?))
+(println (not-empty? []))
+
+;COMP
+(def my-comp (comp inc *))
+(println (my-comp 2 3))
+
+;MEMOIZE
+;Keeps a cache and returns immediately when the same arguments are used
+(defn slow-function
+  [msg]
+  (Thread/sleep 1000)
+  msg)
+(println (slow-function "Done 1"))
+
+(def memoized-slow-function (memoize slow-function))
+(println (memoized-slow-function "Done 2"))
+(println (memoized-slow-function "Done 2"))
+(println (memoized-slow-function "Done 3"))
